98698a7835502591de2a3f812439963ec880f789
Nicholas <nporter108@gmail.com>
2023-03-31 15:54:29 -0400

    Addition of locks

--- src/MyThreadedLife/MyFirstThread.java
+++ src/MyThreadedLife/MyFirstThread.java
@@ -1,24 +1,49 @@
 package MyThreadedLife;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class MyFirstThread implements Runnable{
 
     public String msg;
     public int count;
+    //The following in essence will allow for the threads to at least have a chance for complete execution, that
+    //is, a full print
+    public static Lock conLock = new ReentrantLock();
 
     public MyFirstThread(String m, int c){
         msg = m;
         count = c;
     }
 
-    //Here we see a task which is what will be done upon
+    //Here we see a "task", might be an important piece of lingo which is what will be done upon
     @Override
     public void run() {
 
-        for(int i = 0; i < count; i++){
-            for(int j = 0; i < msg.length(); j++){
-                System.out.println(msg.charAt(j));
+        for(int i = 0; i < count; i++) {
+            //The resource being locked is the console
+            /*Dining philosophers m8
+            * Array of Locks
+            * Array of Philosophers
+            * Philosophers must share two indicies in arrays*/
+            conLock.lock();
+            for (int j = 0; j < msg.length(); j++) {
+
+                System.out.print(msg.charAt(j));
             }
             System.out.println("");
+            conLock.unlock();
+            //After unlocking, we may have other logic to perform:
+            //Sleep inside a for loop causes massive slow-downs for us as well
+            try{
+                //But it allows for even more fair distribution of thread processing
+                Thread.sleep(10);
+                //Yield also hints at a pause, but it's not guaranteed
+                //Thread.yield();
+            }catch (InterruptedException e){
+                System.out.println(e.getMessage());
+            }
+
         }
 
     }
--- src/MyThreadedLife/Threading1.java
+++ src/MyThreadedLife/Threading1.java
@@ -1,19 +1,41 @@
 package MyThreadedLife;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Threading1 {
 
     public static void main(String[] args) {
 
         //Create tasks
-        MyFirstThread x = new MyFirstThread("Hello there friend", 500);
+        MyFirstThread x = new MyFirstThread("Hello there, friend", 500);
         MyFirstThread y = new MyFirstThread("Ech, great", 200);
 
         //Create thread
         Thread t1 = new Thread(x);
         Thread t2 = new Thread(y);
 
-        //BATTLE TO THE DEATH, of threads
         t1.start();
         t2.start();
+        //BATTLE TO THE DEATH, of threads
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+
+            //This executes first, interestingly.
+            System.out.println("OF printings?");
+        }
+
+        tpools();
+    }
+
+    public static void tpools(){
+        ExecutorService ex = Executors.newFixedThreadPool(3);
+        ex.execute(new MyFirstThread("12345",500));
+        ex.execute(new MyFirstThread("asdf",500));
+        ex.execute(new MyFirstThread("!@#$%^&*",500));
+        ex.shutdown();
     }
 }
